425e8a794cffc79a387b88c4dbc77f4110dd4869
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-12-24 13:02:00 +0100

    sm8450-common: wifi-display: Add WfdCommon to boot jars
    Change-Id: I2465b9509d8514f594a62160899a50c23f263b7f

--- common.mk
+++ common.mk
@@ -462,6 +462,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libwfdaac_vendor:64
 
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink \

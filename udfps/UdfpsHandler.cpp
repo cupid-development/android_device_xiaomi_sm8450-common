31a19d804dda67d455cb4b4546b78c389e66916b
Arian <arian.kulmer@web.de>
2022-07-30 17:33:16 +0200

    udfps changes for oss kernel
    Change-Id: I1b6ab4829056c7a97f99bc0c7c59755aa31a5314

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -9,58 +9,31 @@
 #include "UdfpsHandler.h"
 
 #include <android-base/logging.h>
-#include <android-base/unique_fd.h>
 #include <fcntl.h>
 #include <poll.h>
-#include <thread>
 #include <unistd.h>
-
-#define THP_CMD_BASE	1000
-
-enum MODE_TYPE {
-    Touch_Game_Mode				= 0,
-    Touch_Active_MODE      		= 1,
-    Touch_UP_THRESHOLD			= 2,
-    Touch_Tolerance				= 3,
-    Touch_Aim_Sensitivity       = 4,
-    Touch_Tap_Stability         = 5,
-    Touch_Expert_Mode           = 6,
-    Touch_Edge_Filter      		= 7,
-    Touch_Panel_Orientation 	= 8,
-    Touch_Report_Rate      		= 9,
-    Touch_Fod_Enable       		= 10,
-    Touch_Aod_Enable       		= 11,
-    Touch_Resist_RF        		= 12,
-    Touch_Idle_Time        		= 13,
-    Touch_Doubletap_Mode   		= 14,
-    Touch_Grip_Mode        		= 15,
-    Touch_FodIcon_Enable   		= 16,
-    Touch_Nonui_Mode       		= 17,
-    Touch_Debug_Level      		= 18,
-    Touch_Power_Status     		= 19,
-    Touch_Mode_NUM         		= 20,
-    THP_LOCK_SCAN_MODE      	= THP_CMD_BASE + 0,
-    THP_FOD_DOWNUP		      	= THP_CMD_BASE + 1,
-    THP_SET_REPORT_RATE      	= THP_CMD_BASE + 11,
-};
+#include <fstream>
+#include <thread>
 
 #define COMMAND_NIT 10
 #define PARAM_NIT_FOD 1
 #define PARAM_NIT_NONE 0
 
+#define FOD_STATUS_PATH "/sys/devices/platform/soc/990000.spi/spi_master/spi0/spi0.0/fod_status"
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF 0
 
-// defines from drivers/input/touchscreen/xiaomi/xiaomi_touch.h
-#define SET_CUR_VALUE 0
-#define Touch_Fod_Enable 10
+#define FOD_PRESSED_PATH "/sys/devices/platform/soc/990000.spi/spi_master/spi0/spi0.0/fod_down"
 
-#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
-#define TOUCH_ID 0
-#define TOUCH_MAGIC 0x5400
-#define TOUCH_IOC_SETMODE TOUCH_MAGIC + SET_CUR_VALUE
+#define DISP_PARAM_PATH "/sys/devices/virtual/mi_display/disp_feature/disp-DSI-0/disp_param"
+#define DISP_PARAM_LOCAL_HBM_ON "9 1"
+#define DISP_PARAM_LOCAL_HBM_OFF "9 0"
 
-#define FOD_PRESS_STATUS_PATH "/sys/devices/virtual/touch/touch_dev/fod_press_status"
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
 
 static bool readBool(int fd) {
     char c;
@@ -81,14 +54,15 @@ static bool readBool(int fd) {
     return c != '0';
 }
 
-class XiaomiSm8450UdfpsHander : public UdfpsHandler {
+class XiaomiSm8450UdfpsHandler : public UdfpsHandler {
   public:
-    void init(fingerprint_device_t *device) {
+    void init(fingerprint_device_t* device) {
+        LOG(INFO) << __func__;
+
         mDevice = device;
-        touch_fd_ = android::base::unique_fd(open(TOUCH_DEV_PATH, O_RDWR));
 
         std::thread([this]() {
-            int fd = open(FOD_PRESS_STATUS_PATH, O_RDONLY);
+            int fd = open(FOD_PRESSED_PATH, O_RDONLY);
             if (fd < 0) {
                 LOG(ERROR) << "failed to open fd, err: " << fd;
                 return;
@@ -107,50 +81,42 @@ class XiaomiSm8450UdfpsHander : public UdfpsHandler {
                     continue;
                 }
 
-                mDevice->extCmd(mDevice, COMMAND_NIT,
-                                readBool(fd) ? PARAM_NIT_FOD : PARAM_NIT_NONE);
+                bool status = readBool(fd);
 
-                int arg[3] = {TOUCH_ID, THP_FOD_DOWNUP, readBool(fd) ? 1 : 0};
-                ioctl(touch_fd_.get(), TOUCH_IOC_SETMODE, &arg);
+                LOG(INFO) << __func__ << " fod_pressed status: " << status;
 
+                set(DISP_PARAM_PATH, status ? DISP_PARAM_LOCAL_HBM_ON : DISP_PARAM_LOCAL_HBM_OFF);
+                mDevice->extCmd(mDevice, COMMAND_NIT, status ? PARAM_NIT_FOD : PARAM_NIT_NONE);
             }
         }).detach();
     }
 
-    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
-        // nothing
-    }
+    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {}
 
-    void onFingerUp() {
-        // nothing
-    }
+    void onFingerUp() {}
 
     void onAcquired(int32_t result, int32_t vendorCode) {
         LOG(INFO) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
         // vendorCode 21 means waiting for fingerprint
         // result 0 means fingerprint detected successfully
         if (vendorCode == 21 || vendorCode == 22 || vendorCode == 23) {
-            int arg[3] = {TOUCH_ID, Touch_Fod_Enable, FOD_STATUS_ON};
-            ioctl(touch_fd_.get(), TOUCH_IOC_SETMODE, &arg);
+            set(FOD_STATUS_PATH, FOD_STATUS_ON);
         } else if (result == 0) {
-            int arg[3] = {TOUCH_ID, Touch_Fod_Enable, FOD_STATUS_OFF};
-            ioctl(touch_fd_.get(), TOUCH_IOC_SETMODE, &arg);
+            set(FOD_STATUS_PATH, FOD_STATUS_OFF);
         }
     }
 
     void cancel() {
         LOG(INFO) << __func__;
-        int arg[3] = {TOUCH_ID, Touch_Fod_Enable, FOD_STATUS_OFF};
-        ioctl(touch_fd_.get(), TOUCH_IOC_SETMODE, &arg);
+        set(FOD_STATUS_PATH, FOD_STATUS_OFF);
     }
 
   private:
-    fingerprint_device_t *mDevice;
-    android::base::unique_fd touch_fd_;
+    fingerprint_device_t* mDevice;
 };
 
 static UdfpsHandler* create() {
-    return new XiaomiSm8450UdfpsHander();
+    return new XiaomiSm8450UdfpsHandler();
 }
 
 static void destroy(UdfpsHandler* handler) {
@@ -158,6 +124,6 @@ static void destroy(UdfpsHandler* handler) {
 }
 
 extern "C" UdfpsHandlerFactory UDFPS_HANDLER_FACTORY = {
-    .create = create,
-    .destroy = destroy,
+        .create = create,
+        .destroy = destroy,
 };

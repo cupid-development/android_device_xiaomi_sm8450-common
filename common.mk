7a64f59dd7f32eabfbb86b459e2e03bd6df3fd31
Nolen Johnson <johnsonnolen@gmail.com>
2022-08-09 10:54:48 -0400

    sm8450-common: Swap to QTI USB init scripts
    Change-Id: Ibb01a6a8b9817b01a8db397cdf82a89659562d62

--- common.mk
+++ common.mk
@@ -501,6 +501,12 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti \
     libusbhost.vendor
 
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml

df89bb8bfd2f5d7bb884b787f929d74d909b6352
Arian <arian.kulmer@web.de>
2022-10-11 22:01:32 +0200

    sm8450-common: Build hostapd 1.0 HIDL interface
    Change-Id: I73336348b1ca479ea46c4b0de03dee5192a8cff6

--- common.mk
+++ common.mk
@@ -443,6 +443,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    android.hardware.wifi.hostapd@1.0.vendor \
     hostapd \
     hostapd_cli \
     libwifi-hal-qcom \

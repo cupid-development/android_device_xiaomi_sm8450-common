b400ebbb6597845dcb28aceb06c2e316aa90d1ba
Flower sea <zzp060208@gmail.com>
2023-09-24 23:17:23 +0800

    sm8450-common: Add support for mayfly
    Change-Id: I8539b0a06dec4c13bde92a4b0041ce0caa6ae235

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter cupid marble mondrian unicorn zeus,$(TARGET_DEVICE)),)
+ifneq ($(filter cupid marble mayfly mondrian unicorn zeus,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 

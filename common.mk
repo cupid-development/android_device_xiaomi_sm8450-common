d6c95036c843641bb3e3974a34e4195ecc850217
Arian <arian.kulmer@web.de>
2024-01-31 12:56:57 +0100

    sm8450-common: Build qti thermal HAL from source
    Change-Id: Iff18a5fb4a5717aa6b77fdd64a87367dccd5df0b

--- common.mk
+++ common.mk
@@ -436,7 +436,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0.vendor
+    android.hardware.thermal@2.0-service.qti-v2
 
 # Touchscreen
 PRODUCT_COPY_FILES += \

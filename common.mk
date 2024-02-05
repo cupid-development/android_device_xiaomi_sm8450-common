50623401a9eb0f9725c0066ba2a1d51eddabcc99
Jens Reidel <adrian@travitia.xyz>
2024-02-05 04:41:44 +0100

    sm8450-common: Import fingerprint sensor keylayouts from marble V14.0.9.0.TMREUXM
    Change-Id: I70aaedf6225ac556fee608aa76402c5076cf408e
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- common.mk
+++ common.mk
@@ -263,6 +263,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor \

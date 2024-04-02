810255e0063b6bcc01f6de165be8ae409152523c
Jens Reidel <adrian@travitia.xyz>
2024-04-02 10:41:57 +0200

    sm8450-common: Add support for ziyi
    Change-Id: If606dde1ce83461a438951cc41eff1d1e6c7f639
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "cupid marble mayfly mondrian thor unicorn zeus"
+write_headers "cupid marble mayfly mondrian thor unicorn zeus ziyi"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true

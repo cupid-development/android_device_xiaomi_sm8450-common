2875d02e19f93cf7900656c6a22b427700eb5bb9
Arian <arian.kulmer@web.de>
2024-04-16 18:57:54 +0200

    sm8450-common: Update blobs from V816.0.4.0.ULLMIXM
    tunings.txt got removed from vendor, blobs only reference /mnt/vendor/persist/vpp/tunings.txt
    
    Change-Id: Ic572965155a57b6815c7418fb5d368dc9268a960

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -217,7 +217,7 @@ BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-02-01
+VENDOR_SECURITY_PATCH := 2024-03-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk

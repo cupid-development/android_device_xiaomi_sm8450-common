5f52d9cc7ce66a910d60a815c86d248de5e4ea6d
Jens Reidel <adrian@travitia.xyz>
2024-06-05 22:54:52 +0200

    sm8450-common: Add support for diting
    Change-Id: I1222d405c9923da2772b1b5429716d06bb925f54
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter cupid dagda marble mayfly mondrian taranis thor unicorn zeus ziyi zizhan,$(TARGET_DEVICE)),)
+ifneq ($(filter cupid dagda diting marble mayfly mondrian taranis thor unicorn zeus ziyi zizhan,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "cupid dagda marble mayfly mondrian taranis thor unicorn zeus ziyi zizhan"
+write_headers "cupid dagda diting marble mayfly mondrian taranis thor unicorn zeus ziyi zizhan"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true

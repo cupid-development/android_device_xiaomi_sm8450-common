e6b78874af68d3fe1955fd9e6baecdf83c31a434
Arian <arian.kulmer@web.de>
2024-06-09 11:07:21 +0200

    sm8450-common: udfps: Kill dedicated enroll handling
    Change-Id: I906dbfd2b4fbe2c8d28bda6927e70144350f9ee9

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -218,22 +218,6 @@ class XiaomiSm8450UdfpsHander : public UdfpsHandler {
 
     void cancel() {
         LOG(DEBUG) << __func__;
-        enrolling = false;
-    }
-
-    void preEnroll() {
-        LOG(DEBUG) << __func__;
-        enrolling = true;
-    }
-
-    void enroll() {
-        LOG(DEBUG) << __func__;
-        enrolling = true;
-    }
-
-    void postEnroll() {
-        LOG(DEBUG) << __func__;
-        enrolling = false;
     }
 
   private:
@@ -241,7 +225,6 @@ class XiaomiSm8450UdfpsHander : public UdfpsHandler {
     android::base::unique_fd touch_fd_;
     android::base::unique_fd disp_fd_;
     bool isFpcFod;
-    bool enrolling = false;
     uint32_t lastPressX, lastPressY;
 
     void setFingerDown(bool pressed) {

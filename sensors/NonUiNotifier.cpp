27ed913261151d7b2a999ff78c67d872a0643676
Arian <arian.kulmer@web.de>
2024-07-24 09:21:04 +0200

    sm8450-comomn: libsensorndkbridge nonui-notfier
    Change-Id: Iee5a5a61d276b8ed36038aa1d3c0897dc1663813

--- sensors/NonUiNotifier.cpp
+++ sensors/NonUiNotifier.cpp
@@ -10,8 +10,9 @@
 #include <android-base/properties.h>
 #include <android-base/unique_fd.h>
 
-#include <android/frameworks/sensorservice/1.0/ISensorManager.h>
-#include <android/frameworks/sensorservice/1.0/types.h>
+#include <android/looper.h>
+#include <android/sensor.h>
+#include <utils/Errors.h>
 
 #include <fcntl.h>
 #include <poll.h>
@@ -20,19 +21,10 @@
 
 #include "xiaomi_touch.h"
 
+using ::android::NO_ERROR;
+
 #define SENSOR_TYPE_XIAOMI_SENSOR_NONUI 33171027
 #define SENSOR_NAME_XIAOMI_SENSOR_NONUI "xiaomi.sensor.nonui"
-
-using ::android::frameworks::sensorservice::V1_0::IEventQueue;
-using ::android::frameworks::sensorservice::V1_0::IEventQueueCallback;
-using ::android::frameworks::sensorservice::V1_0::ISensorManager;
-using ::android::frameworks::sensorservice::V1_0::Result;
-using ::android::hardware::Return;
-using ::android::hardware::Void;
-using ::android::hardware::sensors::V1_0::Event;
-using ::android::hardware::sensors::V1_0::SensorType;
-
-using android::sp;
 
 #define TOUCH_DEV_PATH "/dev/xiaomi-touch"
 #define TOUCH_MAGIC 'T'
@@ -60,104 +52,73 @@ static bool readBool(int fd) {
     return c != '0';
 }
 
-struct NonUiSensorCallback : IEventQueueCallback {
-    Return<void> onEvent(const Event& e) {
-        /* handle sensor event e */
-        LOG(ERROR) << "onEvent scalar: " << e.u.scalar;
-        bool nonUi = e.u.scalar == 1;
-
-        // android::base::unique_fd touch_fd_;
-        // touch_fd_ = android::base::unique_fd(open(TOUCH_DEV_PATH, O_RDWR));
-
-        int buf[MAX_BUF_SIZE] = {0, Touch_Nonui_Mode, nonUi ? 2 : 0};
-        ioctl(open(TOUCH_DEV_PATH, O_RDWR), TOUCH_IOC_SET_CUR_VALUE, &buf);
-
-        return Void();
-    }
-};
-
 }  // namespace
 
+struct SensorContext {
+    ASensorEventQueue *queue;
+};
+
+int NonUiSensorCallback(__attribute__((unused)) int fd, __attribute__((unused)) int events,
+                    void *data) {
+    ASensorEvent event;
+    int event_count = 0;
+    SensorContext *context = reinterpret_cast<SensorContext *>(data);
+    event_count = ASensorEventQueue_getEvents(context->queue, &event, 1);
+
+    LOG(INFO) << "sensor reported: 0:" << event.data[0] << ", 1: " << event.data[1] << ", 2: " << event.data[2] << ", 3: " << event.data[3];
+
+    return 1;
+}
+
 int main() {
-    sp<ISensorManager> manager = ISensorManager::getService();
-    if (manager == nullptr) {
-        LOG(ERROR) << "failed to get ISensorManager";
+    ASensorManager *sensorManager = nullptr;
+    ASensorList sensorList;
+    ASensorRef NonUiSensor = nullptr;
+    ALooper *looper;
+    struct SensorContext context = {nullptr};
+    int err = NO_ERROR;
+
+    sensorManager = ASensorManager_getInstanceForPackage("");
+    if (!sensorManager) {
+        LOG(ERROR) << "Failed to get ASensorManager instance";
+        return 0;
     }
 
-    int32_t sensorHandle = -1;
-    manager->getDefaultSensor(static_cast<SensorType>(SENSOR_TYPE_XIAOMI_SENSOR_NONUI),
-                              [&sensorHandle](const auto& info, auto r) {
-                                    sensorHandle = info.sensorHandle;
-                              });
-
-    sp<IEventQueue> queue;
-    Result res;
-    manager->createEventQueue(new NonUiSensorCallback(), [&queue, &res](const auto& q, auto r) {
-        queue = q;
-        res = r;
-    });
-    if (res != Result::OK) {
-        return EXIT_FAILURE;
+    size_t size = ASensorManager_getSensorList(sensorManager, &sensorList);
+    for (int i = 0; i < size; i++) {
+        if (strcmp(ASensor_getStringType(sensorList[i]), SENSOR_NAME_XIAOMI_SENSOR_NONUI) == 0 && ASensor_isWakeUpSensor(sensorList[i])) {
+            NonUiSensor = sensorList[i];
+            LOG(ERROR) << "found sensor: " << ASensor_getName(sensorList[i]) << " type: " << ASensor_getStringType(sensorList[i]) << " wakeup: " << ASensor_isWakeUpSensor(sensorList[i]) << " reporting mode: " << ASensor_getReportingMode(sensorList[i]);
+            break;
+        }
     }
-
-    // Enable states of touchscreen sensors
-    const std::vector<const char*> paths = {
-            "/sys/class/touch/touch_dev/fod_longpress_gesture_enabled",
-            "/sys/class/touch/touch_dev/gesture_single_tap_enabled",
-            "/sys/class/touch/touch_dev/gesture_double_tap_enabled"};
-
-    pollfd* pollfds = new pollfd[paths.size()];
-    for (size_t i = 0; i < paths.size(); ++i) {
-        int fd = open(paths[i], O_RDONLY);
-        if (fd < 0) {
-            LOG(ERROR) << "failed to open " << paths[i] << " , err: " << fd;
-            return EXIT_FAILURE;
+    if (NonUiSensor == nullptr) {
+        LOG(ERROR) << "Failed to get wake-up version of " << SENSOR_NAME_XIAOMI_SENSOR_NONUI;
+        return 0;
+    } else {
+        looper = ALooper_forThread();
+        if (looper == nullptr) {
+            looper = ALooper_prepare(ALOOPER_PREPARE_ALLOW_NON_CALLBACKS);
         }
+        context.queue =
+            ASensorManager_createEventQueue(sensorManager, looper, 0, NonUiSensorCallback, &context);
 
-        pollfds[i].fd = fd;
-        pollfds[i].events = POLLERR | POLLPRI;
-        pollfds[i].revents = 0;
-    }
-
-    while (true) {
-        int rc = poll(pollfds, paths.size(), -1);
-        LOG(ERROR) << "poll: " << rc;
-        if (rc < 0) {
-            LOG(ERROR) << "failed to poll, err: " << rc;
-            continue;
-        }
-
-        for (size_t i = 0; i < paths.size(); ++i) {
-            if (pollfds[i].revents & (POLLERR | POLLPRI)) {
-                LOG(ERROR) << "Event on " << paths[i];
-            }
-        }
-
-        bool enabled = false;
-        for (size_t i = 0; i < paths.size(); ++i) {
-            enabled = enabled || readBool(pollfds[i].fd);
-        }
-        LOG(ERROR) << "got notified about poll, enabled: " << enabled;
-        if (enabled) {
-            res = queue->enableSensor(sensorHandle, 20000 /* sample period */, 0 /* latency */);
-            if (res != Result::OK) {
-                LOG(ERROR) << "failed to enable sensor";
-            }
+        err = ASensorEventQueue_registerSensor(context.queue, NonUiSensor, 0, 0);
+        if (err != NO_ERROR) {
+            LOG(ERROR) << "Failed to register sensor with event queue, error: " << err;
+            return 0;
         } else {
-            res = queue->disableSensor(sensorHandle);
-            if (res != Result::OK) {
-                LOG(ERROR) << "failed to disable sensor";
+            while (true) {
+                ALooper_pollOnce(-1, nullptr, nullptr, nullptr);
+                LOG(ERROR) << "ALooper_pollOnce returned";
             }
         }
     }
 
-    /*
-     * Free the event queue.
-     * kernel calls decStrong() on server side implementation of IEventQueue,
-     * hence resources (including the callback) are freed as well.
-     */
-    queue = nullptr;
+    if (sensorManager != nullptr && context.queue != nullptr) {
+        ASensorEventQueue_disableSensor(context.queue, NonUiSensor);
+        ASensorManager_destroyEventQueue(sensorManager, context.queue);
+    }
 
-    // Should never reach this
-    return EXIT_SUCCESS;
+    return 0;
 }

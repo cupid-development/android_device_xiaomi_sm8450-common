53191f28114a9430024c2ab14f31b68856288cb5
Arian <arian.kulmer@web.de>
2024-07-27 15:11:24 +0200

    sm8450-common: sensors: Convert nonui notifier into a generalized sensor based notifier
    Change-Id: I34b9861341f4f033185fdbd066b46d779dd95cfa

---
+++ sensors/main.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "SensorNotifier"
+
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+
+#include "SensorNotifierExt.h"
+#include "notifiers/NonUiNotifier.h"
+
+int main() {
+    sp<ISensorManager> manager = ISensorManager::getService();
+    if (manager == nullptr) {
+        LOG(ERROR) << "failed to get ISensorManager";
+        return EXIT_FAILURE;
+    }
+
+    std::vector<std::unique_ptr<SensorNotifier>> notifiers;
+    notifiers.push_back(std::make_unique<NonUiNotifier>(manager));
+    for (const auto& notifier : notifiers) {
+        notifier->activate();
+    }
+
+    std::unique_ptr<SensorNotifierExt> sensorNotifierExt =
+            std::make_unique<SensorNotifierExt>(manager);
+    for (const auto& notifier : sensorNotifierExt->mNotifiers) {
+        notifier->activate();
+    }
+
+    while (true) {
+        // Sleep to keep the notifiers alive
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+
+    // Should never reach this
+    return EXIT_SUCCESS;
+}

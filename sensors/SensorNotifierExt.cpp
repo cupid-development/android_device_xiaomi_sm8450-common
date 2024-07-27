7eb76620238a6180d3897de866e4194e17ebc4a1
Arian <arian.kulmer@web.de>
2024-07-27 17:41:54 +0200

    sm8450-common: sensors: Add support for device specific extensions
    Change-Id: Ic983648ae0ec023c87af6c34be4fe98883218e93

---
+++ sensors/SensorNotifierExt.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "SensorNotifierExt.h"
+
+SensorNotifierExt::SensorNotifierExt(sp<ISensorManager> /* manager */,
+                                     process_msg_t /* processMsg */) {}

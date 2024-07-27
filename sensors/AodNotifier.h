b6891875fa071f7dc129b28d2c5be6aa7fd08712
Arian <arian.kulmer@web.de>
2024-07-27 16:06:00 +0200

    sm8450-common: sensors: Add aod notifier
    Change-Id: I2ba35d7b7db4e6922d83f041497fe66bf52124cb

---
+++ sensors/AodNotifier.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "SensorNotifier.h"
+
+class AodNotifier : public SensorNotifier {
+  public:
+    AodNotifier(sp<ISensorManager> manager);
+    ~AodNotifier();
+
+  protected:
+    void pollingFunction();
+};

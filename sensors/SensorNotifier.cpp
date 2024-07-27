189c20c8b1213e63ceff0feb7d7e3135309e5565
Arian <arian.kulmer@web.de>
2024-07-27 15:11:24 +0200

    sm8450-common: sensors: Convert nonui notifier into a generalized sensor based notifier
    Change-Id: I34b9861341f4f033185fdbd066b46d779dd95cfa

---
+++ sensors/SensorNotifier.cpp
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "SensorNotifier"
+
+#include <android-base/logging.h>
+
+#include "SensorNotifier.h"
+
+using android::hardware::sensors::V1_0::SensorFlagBits;
+using android::hardware::sensors::V1_0::SensorInfo;
+
+SensorNotifier::SensorNotifier(sp<ISensorManager> manager) : mManager(manager) {}
+
+SensorNotifier::~SensorNotifier() {
+    if (mQueue != nullptr) {
+        /*
+         * Free the event queue.
+         * kernel calls decStrong() on server side implementation of IEventQueue,
+         * hence resources (including the callback) are freed as well.
+         */
+        mQueue = nullptr;
+    }
+}
+
+Result SensorNotifier::initializeSensorQueue(std::string typeAsString, bool wakeup,
+                                             sp<IEventQueueCallback> callback) {
+    Result res;
+    std::vector<SensorInfo> sensorList;
+
+    mManager->getSensorList([&sensorList, &res](const auto& l, auto r) {
+        sensorList = l;
+        res = r;
+    });
+    if (res != Result::OK) {
+        LOG(ERROR) << "failed to get sensors list";
+        return res;
+    }
+    auto it = std::find_if(sensorList.begin(), sensorList.end(),
+                           [this, &typeAsString, &wakeup](const SensorInfo& sensor) {
+                               return (sensor.typeAsString == typeAsString) &&
+                                      ((sensor.flags & SensorFlagBits::WAKE_UP) == wakeup);
+                           });
+
+    if (it != sensorList.end()) {
+        mSensorHandle = it->sensorHandle;
+    } else {
+        LOG(ERROR) << "failed to get " << typeAsString << " sensor with wake-up: " << wakeup;
+        return Result::NOT_EXIST;
+    }
+
+    mManager->createEventQueue(callback, [this, &res](const auto& q, auto r) {
+        this->mQueue = q;
+        res = r;
+    });
+    if (res != Result::OK) {
+        LOG(ERROR) << "failed to create event queue";
+        return res;
+    }
+
+    return Result::OK;
+}
+
+void SensorNotifier::activate() {
+    if (mActive) {
+        return;
+    }
+    mActive = true;
+    mPollingThread = std::thread(&SensorNotifier::pollingFunction, this);
+}
+
+void SensorNotifier::deactivate() {
+    if (!mActive) {
+        return;
+    }
+    mActive = false;
+    if (mPollingThread.joinable()) {
+        mPollingThread.join();
+    }
+    if (mQueue != nullptr) {
+        mQueue->disableSensor(mSensorHandle);
+    }
+}

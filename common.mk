927435083809883ece29796467cc9294658bc191
AdarshGrewal <adarshgrewal@gmail.com>
2024-08-07 01:01:15 +0530

    sm8450-common: Build libsndcardparser from source
    Change-Id: I338a79e8c5e4cfe4070e249f952b6c6b18ca6202

--- common.mk
+++ common.mk
@@ -85,6 +85,7 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle:64 \
     libqcomvisualizer:64 \
     libqcomvoiceprocessing:64 \
+    libsndcardparser:64 \
     libtinycompress:64 \
     libvolumelistener:64
 

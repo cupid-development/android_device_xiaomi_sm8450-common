d89b9b37508b26aae39fcd8c5bb33a52bd878859
AdarshGrewal <adarshgrewal@gmail.com>
2024-08-07 00:28:40 +0530

    sm8450-common: Build audioadsprpcd from source
    Change-Id: I19d38c534edf26886bd354548eaee661dd79f793

--- common.mk
+++ common.mk
@@ -68,6 +68,9 @@ PRODUCT_PACKAGES += \
     audio.usb.default:64
 
 PRODUCT_PACKAGES += \
+    audioadsprpcd
+
+PRODUCT_PACKAGES += \
     libaudioroute.vendor:64 \
     libqcompostprocbundle:64 \
     libqcomvisualizer:64 \

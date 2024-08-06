bf17172e44ed275cc4d7904453a0b1ea4c3161a7
AdarshGrewal <adarshgrewal@gmail.com>
2024-08-07 01:00:02 +0530

    sm8450-common: Build libbatterylistener from source
    Change-Id: I8d3171869764e50544a5fcd7e793589e2ecbabc9

--- common.mk
+++ common.mk
@@ -79,6 +79,7 @@ PRODUCT_PACKAGES += \
     libagmclient:64 \
     libagmmixer:64 \
     libaudioroute.vendor:64 \
+    libbatterylistener:64 \
     libfmpal:64 \
     libpalclient:64 \
     libqcompostprocbundle:64 \

4978848b1c1f4f74c02a821a230e9eba07e833f5
Aaron Kling <webgeek1234@gmail.com>
2024-08-14 01:44:45 -0500

    sm8450-common: Enable elf checks
    Change-Id: Ia6c9a381abb3384c77532d75e02fd84c841a4d1e

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -13,6 +13,7 @@ MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
+export TARGET_ENABLE_CHECKELF="true"
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then

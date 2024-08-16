772d22ff4bb8d784beb8dacfd1876cd24c3b1a34
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 02:04:34 +0300

    sm8450-common: Fix vendor.qti.diaghal@1.0 ELF checks
    Change-Id: Idf2919284e9843e4f7d2c1b903bbffd04ded0863

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,7 +43,8 @@ function lib_to_package_fixup_vendor_variants() {
         vendor.qti.hardware.qccsyshal@1.0 | \
         vendor.qti.hardware.qccsyshal@1.1 | \
         vendor.qti.hardware.qccvndhal@1.0 | \
-        vendor.qti.imsrtpservice@3.0)
+        vendor.qti.imsrtpservice@3.0 | \
+        vendor.qti.diaghal@1.0)
             echo "$1-vendor"
             ;;
         *)

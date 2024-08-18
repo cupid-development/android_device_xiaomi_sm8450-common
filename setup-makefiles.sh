667467ac57f5053025933a7439bc810ffed71dfa
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    sm8450-common: Skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -50,6 +50,10 @@ function lib_to_package_fixup_vendor_variants() {
         com.qualcomm.qti.dpm.api@1.0)
             echo "$1-vendor"
             ;;
+        libpalclient | \
+        libwpa_client | \
+        libwfdaac_vendor)
+            ;;
         *)
             return 1
     esac

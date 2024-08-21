9eac19e128c862e3428b25d2f89b5ad337529afe
Arian <arian.kulmer@web.de>
2024-08-21 18:05:17 +0200

    sm8450-common: Don't include media_codecs_dolby_audio
    Change-Id: Ia0dc1e6c1ddd91f06a20cec8312663ba77546bda

--- extract-files.sh
+++ extract-files.sh
@@ -70,6 +70,9 @@ function blob_fixup() {
         vendor/etc/init/init.embmssl_server.rc)
             sed -i -n '/interface/!p' "${2}"
             ;;
+        vendor/etc/media_codecs_c2_audio.xml)
+            sed -i '/media_codecs_dolby_audio/d' "${2}"
+            ;;
         vendor/etc/qcril_database/upgrade/config/6.0_config.sql)
             sed -i '/persist.vendor.radio.redir_party_num/ s/true/false/g' "${2}"
             ;;

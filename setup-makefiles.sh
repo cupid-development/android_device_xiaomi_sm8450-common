43d1733ed52a3557f3f7e4eab929b45bef6cb8c1
Michael Bestas <mkbestas@lineageos.org>
2024-09-18 13:34:59 +0300

    sm8450-common: Enable ELF checks for libwfdmmsrc_proprietary
    Change-Id: I7eecffc5481b44451094a6816e50436ad084db7a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -53,8 +53,7 @@ function lib_to_package_fixup_vendor_variants() {
         libagm | \
         libar-pal | \
         libpalclient | \
-        libwpa_client | \
-        libwfdaac_vendor)
+        libwpa_client)
             ;;
         *)
             return 1

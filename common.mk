968f5296fd833fdec85479ea64dcf6592c3a5088
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:46 +0300

    sm8450-common: Switch to AIDL USB HALs
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- common.mk
+++ common.mk
@@ -382,8 +382,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \

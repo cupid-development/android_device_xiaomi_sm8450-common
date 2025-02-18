0229b22b292006d16a3d65949b9d2fec275a6428
Arian <arian.kulmer@web.de>
2025-02-18 15:06:15 +0100

    sm8450-common: Move stock qcril sql migrations to vendor again
    Change-Id: Ied6f5db323ead3de9f891a1fef009a3ef53e99a3

--- qcril_database/config/12.0_config.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Copyright (c) 2022 Qualcomm Technologies, Inc.
-  All Rights Reserved.
-  Confidential and Proprietary - Qualcomm Technologies, Inc.
-*/
-
-CREATE TABLE IF NOT EXISTS qcril_properties_table (property TEXT PRIMARY KEY NOT NULL, def_val TEXT, value TEXT);
-INSERT OR REPLACE INTO qcril_properties_table(property, def_val) VALUES('qcrildb_version',12.0);
--- qcril_database/config/8.0_config.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Copyright (c) 2021 Qualcomm Technologies, Inc.
-  All Rights Reserved.
-  Confidential and Proprietary - Qualcomm Technologies, Inc.
-*/
-
-CREATE TABLE IF NOT EXISTS qcril_properties_table (property TEXT PRIMARY KEY NOT NULL, def_val TEXT, value TEXT);
-INSERT OR REPLACE INTO qcril_properties_table(property, def_val) VALUES('qcrildb_version',8.0);
-UPDATE qcril_properties_table SET def_val="1" WHERE property="persist.vendor.radio.cs_srv_type";

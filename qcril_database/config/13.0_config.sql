81a19757c02ba90efd398414ea42eb3e7083213d
Arian <arian.kulmer@web.de>
2025-02-18 15:06:15 +0100

    sm8450-common: Move stock qcril sql migrations to vendor again
    Change-Id: Ied6f5db323ead3de9f891a1fef009a3ef53e99a3

--- qcril_database/config/13.0_config.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Copyright (c) 2022 Qualcomm Technologies, Inc.
-  All Rights Reserved.
-  Confidential and Proprietary - Qualcomm Technologies, Inc.
-*/
-
-CREATE TABLE IF NOT EXISTS qcril_properties_table (property TEXT PRIMARY KEY NOT NULL, def_val TEXT, value TEXT);
-INSERT OR REPLACE INTO qcril_properties_table(property, def_val) VALUES('qcrildb_version',13.0);
-INSERT OR REPLACE INTO qcril_properties_table(property, def_val) VALUES("persist.vendor.radio.force_gba_over_isim_app", "false");
